83299aca733a718d01fc99494970c241b7bcf352
Carlosrnes <31324633+Carlosrnes@users.noreply.github.com>
2022-05-26 13:39:26 +0100

    Split final dataframe

--- tsp.py
+++ tsp.py
@@ -71,18 +71,21 @@ gens = [500]
 selections = [fps, tournament, rank]
 crossovers = [pmx_co, cycle_co]
 mutations = [inversion_mutation, swap_mutation]
-co_ps = [0.9]
-mu_ps = [0.1]
+co_ps = [0.8, 0.9]
+mu_ps = [0.1, 0.2]
 elitism = [True, False]
+
 
 # combination of all possible settings to the tsp problem
 comb_settings = np.array(np.meshgrid(gens, selections, crossovers, mutations, co_ps, mu_ps, elitism)).T.reshape(-1, 7)
 
 
 def evaluate(settings, runs=30, path='output/test50.csv'):
-    df_final = pd.DataFrame(columns=['run', 'settings', 'generation', 'fitness'])
+    df_final = pd.DataFrame(columns=['run', 'gens', 'select', 'crossover', 'mutate', \
+                                     'co_p', 'mu_p', 'elitism', 'generation', 'fitness', 'time'])
     for run in range(1, runs + 1):
         for setting in settings:
+            start = time.time()
             comb_set = f"gens: {setting[0]}; select: {str(setting[1]).split()[1]}; \
                         crossover: {str(setting[2]).split()[1]}; mutate: {str(setting[3]).split()[1]}; \
                         co_p: {setting[4]}; mu_p: {setting[5]}; elitism: {setting[6]}"
@@ -103,22 +106,31 @@ def evaluate(settings, runs=30, path='output/test50.csv'):
                                                 elitism=setting[6]
             )
             df = pd.DataFrame()
+            df['run'] = run
+            df['gens'] = setting[0]
+            df['select'] = str(setting[1]).split()[1]
+            df['crossover'] = str(setting[2]).split()[1]
+            df['mutate'] = str(setting[3]).split()[1]
+            df['co_p']=setting[4]
+            df['mu_p'] = setting[5]
+            df['elitism'] = setting[6]
             df['generation'] = list_gen
             df['fitness'] = list_fitness
-            df['settings'] = comb_set
-            df['run'] = run
-            df = df[['run', 'settings', 'generation', 'fitness']]
-            df_final = pd.concat([df_final, df])
+            df['time'] = time.time() - start
+            df = df[['run', 'gens', 'select', 'crossover', 'mutate', 'co_p', 'mu_p', 'elitism', 'generation', 'fitness', 'time']]
+            df_final = df_final.append(df)
 
     df_final.to_csv(path, sep=';', index=False)
 
-    df_plot = pd.read_csv(path, sep=';')
-    fig, ax = plt.subplots(1, 1, figsize=(15, 10))
-    sns.lineplot(data=df_plot, x='generation', y='fitness', hue='settings', estimator='mean', ci=99, err_style='band', ax=ax, legend=True)
-    plt.legend(bbox_to_anchor=(0, -0.1), loc='upper left', borderaxespad=0)
-    return plt.show()  # len(df_final) # settings, list_gen, list_fitness
-
+    return print('Done')  # len(df_final) # settings, list_gen, list_fitness
 
 evaluate(settings=comb_settings, runs=30)
 
+
+# plot all combinations
+df_plot = pd.read_csv(path, sep=';')
+fig, ax = plt.subplots(1, 1, figsize=(15, 10))
+sns.lineplot(data=df_plot, x='generation', y='fitness', hue='settings', estimator='mean', ci=99, err_style='band', ax=ax, legend=True)
+plt.legend(bbox_to_anchor=(0, -0.1), loc='upper left', borderaxespad=0)
+plt.show()
 
